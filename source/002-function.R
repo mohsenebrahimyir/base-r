814d5f400f1f5f9052efa8834a2b16e8b3c28fe8
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-11-18 00:29:07 +0330

    update

---
+++ source/002-function.R
@@ -0,0 +1,12 @@
+jam <- function(x, y) x + y
+
+k = 4
+kam <- function(x, y) {
+  z = 6
+  (x - y) * k * z
+}
+
+salam <- function(name = "Mohsen") {
+  salam_name <- paste("Salam", name, "aziz")
+  return(salam_name)
+}
