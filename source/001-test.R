dde333ac14c127be734c7516370cd2042cac0c6e
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-11-17 17:55:03 +0330

    update

--- source/001-test.R
+++ source/001-test.R
@@ -1,6 +1,23 @@
+# intro ----
 setwd("~/Desktop")
 
 # this is first code
 print("line 1")
 
 print("line 2")
+
+# variable ####
+
+line_2 <- "line-2"
+
+`line-2` <- 2 
+
+print(line)
+
+
+
+
+
+
+
+
