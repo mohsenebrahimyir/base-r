5f0a87dc27ed186df90fde37599ffae62cb01507
mohsenebrahimyir <mohsenebrahimy.ir@gmail.com>
2023-08-20 23:54:34 +0330

    update

---
+++ exercise/address@email.com/project/table-1-demographic/code.R
@@ -0,0 +1,12 @@
+solution = function(x) {
+  # begin your code --------------
+  
+  
+  
+  
+  
+  
+  # end your code ----------------
+}
+
+if (sys.nframe() == 0) "stdin" |> readLines(1) |> solution() |> cat("\n")
